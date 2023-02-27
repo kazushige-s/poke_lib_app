16165a5232ea229dde870fb76c23183415977630
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-02-27 15:24:48 +0900

    ✨ Promiseでデータを取得した。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
+import { getAllPokemon } from '@/utils/pokemon';
 import { Inter } from 'next/font/google'
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData=async()=>{
+      //すべてのポケモンデータを取得
+      const res = await getAllPokemon(initialURL);
+      console.log(res);
+    }
+    fetchPokemonData();
+  }, []);
+
   return (
     <div>test</div>
   )
---
+++ src/utils/pokemon.ts
@@ -0,0 +1,12 @@
+//関数getAllPokemonを定義
+export const getAllPokemon = (url: string) => {
+  //Promiseオブジェクトを返す
+  return new Promise((resolve, reject) => {
+    //fetchメソッドでurlを取得
+    fetch(url)
+      //resで受け取ったデータをjson形式に変換
+      .then((res) => res.json())
+      //json形式のデータをdataとして受け取り、resolve(data)でPromiseオブジェクトを返す
+      .then((data) => resolve(data));
+  });
+};
