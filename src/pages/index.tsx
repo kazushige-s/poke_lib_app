e79a8fbef1e3cbd7e0b1c97a91748a9f6af4e147
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-02-27 16:17:16 +0900

    ✨ ローディングを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { getAllPokemon } from '@/utils/pokemon';
 import { Inter } from 'next/font/google'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,16 +8,24 @@ export default function Home() {
 
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
+  //ロード中かどうかの状態
+  const [loading, setLoading] = useState(true);
+
+  //初回レンダリング時に実行
   useEffect(() => {
     const fetchPokemonData=async()=>{
       //すべてのポケモンデータを取得
       const res = await getAllPokemon(initialURL);
       console.log(res);
+      //ロード中の状態を解除
+      setLoading(false);
     }
     fetchPokemonData();
   }, []);
 
   return (
-    <div>test</div>
+    <div>
+      {loading ? <h1>loading...</h1> : <h1>データを取得しました</h1>}
+    </div>
   )
 }
